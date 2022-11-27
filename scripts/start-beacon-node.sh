3036d87db84bf3095b068a5b1e313deb1a2d2e4e
Tiger Chan <tigerhk@gmail.com>
2022-11-27 20:18:56 +0800

    update option

--- scripts/start-beacon-node.sh
+++ scripts/start-beacon-node.sh
@@ -59,7 +59,7 @@ exec lighthouse \
 	--eth1-endpoints $VOTING_ETH1_NODES \
 	--http \
 	--execution-endpoints=http://geth:8551
-	--execution-jwt-secret-key=/geth/jwtsecret
+	--execution-jwt=/geth/jwtsecret
 	--http-address 0.0.0.0 \
 	$METRICS_PARAMS \
 	$GRAFFITI_PARAM \

4af86748815e3a9aee9c6bace918c99560d014e8
Tiger Chan <tigerhk@gmail.com>
2022-11-27 20:35:01 +0800

    -

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -11,8 +11,8 @@ fi
 
 if [ "$START_GETH" != "" ]; then
 	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
-		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net"
+		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --authrpc.jwtsecret /root/.ethereum/geth/jwtsecret
 	else
-		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net"
+		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --authrpc.jwtsecret /root/.ethereum/geth/jwtsecret
 	fi
 fi

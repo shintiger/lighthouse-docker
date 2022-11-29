8189f387f8301f63b3a4be722103f47d7baae8ab
Tiger <tiger@gogochart.com>
2022-11-29 18:54:13 +0800

    switch to fast sync

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -11,8 +11,8 @@ fi
 
 if [ "$START_GETH" != "" ]; then
 	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
-		exec geth --cache=2048 --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --authrpc.addr 0.0.0.0 --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/.ethereum/geth/jwtsecret
+		exec geth --fast --cache=2048 --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --authrpc.addr 0.0.0.0 --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/.ethereum/geth/jwtsecret
 	else
-		exec geth --cache=2048 --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --authrpc.addr 0.0.0.0 --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/.ethereum/geth/jwtsecret
+		exec geth --fast --cache=2048 --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --authrpc.addr 0.0.0.0 --authrpc.port 8551 --authrpc.vhosts=* --authrpc.jwtsecret /root/.ethereum/geth/jwtsecret
 	fi
 fi
